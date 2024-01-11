4bb90566493c36341c1cb12a32765ac852906da7
damir-dev-21 <devdamiricher@gmail.com>
2024-01-11 14:43:59 +0500

    add-post-data

--- src/pages/add_employee_page.js
+++ src/pages/add_employee_page.js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import { useState } from 'react'
 
 function AddEmployee() {
@@ -5,6 +6,7 @@ function AddEmployee() {
     const [name, setName] = useState('')
     const [surName, setSurname] = useState('')
     const [lastName, setLastName] = useState('')
+    const [dateOfBirthday, setDateOfBirthday] = useState('')
     const [post, setPost] = useState('')
     const [passportSerial, setPassportSerial] = useState('')
     const [passportNumber, setPassportNumber] = useState(0)
@@ -12,6 +14,12 @@ function AddEmployee() {
     const [file1Base64String, setFile1Base64String] = useState('');
     const [file2Base64String, setFile2Base64String] = useState('');
     const [file3Base64String, setFile3Base64String] = useState('');
+    const [modalVisible, setModalVisible] = useState(false);
+    const [modalLoadingVisible, setModalLoadingVisible] = useState(false);
+    const [modalSuccessVisible, setModalSuccessVisible] = useState(false);
+    const [modalInfoVisible, setModalInfoVisible] = useState(false);
+
+    let _files = []
 
     const handleFileChange = (event) => {
         const file = event.target.files[0];
@@ -46,22 +54,91 @@ function AddEmployee() {
         }
     };
 
+    const getData = async () => {
+       
+        try {
+            setModalVisible(true);
+            
+            
+            let responce = await axios.post('https://siyobzup.xyz/api/data/', { "pasSer": passportSerial, "pasNum": passportNumber, "pasDob": dateOfBirthday})
+            if(responce.status == 200){
+                let extractedData = responce.data;
+                setName(extractedData['data']['firstName'])
+                setSurname(extractedData['data']['surName'])
+                setLastName(extractedData['data']['middleName'])
+                setInps(extractedData['data']['personalNum'])
+                console.log(extractedData);
+            }else{
 
+            }
+        } catch (error) {
+            setModalVisible(false)
+            console.log(error);
+        }
+        setModalVisible(false)
+    }
+
+    const postData = async () => {
+        if(name == '' || surName == '' || lastName == '' || post == '' || inps == '' || passportNumber == '' || passportNumber == 0 || dateOfBirthday == ''){
+            setModalInfoVisible(true)
+            return
+        }
+
+        try {
+            setModalLoadingVisible(true);
+
+
+            let responce = await axios.post('https://siyobzup.xyz/api/create/', { 
+                "name":name,
+                "surname":surName,
+                "lastname":lastName,
+                "post": post,
+                "inps":inps,
+                "pasport_serial": passportSerial,
+                "passport_number":passportNumber,
+                "files":_files
+             })
+            if (responce.status == 200) {
+                let extractedData = responce.data;
+                setModalLoadingVisible(false);
+                const interval = setInterval(() => {
+                    setModalVisible(false);
+                }, 3000);
+                clearInterval(interval)
+
+                console.log(extractedData);
+            } else {
+
+            }
+        } catch (error) {
+            setModalLoadingVisible(false)
+            console.log(error);
+        }
+        setModalLoadingVisible(false)
+    }
 
     return (
         <div>
+            {modalVisible && <Modal onClose={() => setModalVisible(false)} />}
+            {modalLoadingVisible && <ModalLoading onClose={() => setModalVisible(false)} />}
+            {modalSuccessVisible && <ModalSuccess onClose={() => setModalVisible(false)} />}
+
             <div class="form-container">
-                <input type="text" id="firstname" name="firstname" placeholder="Имя" />
-                <input type="text" id="lastname" name="lastname" placeholder="Фамилия" />
-                <input type="text" id="patronymic" name="patronymic" placeholder="Отчество" />
+                <input type="text" id="firstname" name="firstname" value={name} placeholder="Имя" />
+                <input type="text" id="lastname" name="lastname" value={surName} placeholder="Фамилия" />
+                <input type="text" id="patronymic" name="patronymic" value={lastName} placeholder="Отчество" />
                 <input type="text" id="position" name="position" placeholder="Должность" />
 
                 <div class="row">
-                    <input type="text" id="passport-series" name="passport-series" placeholder="Серия паспорта" />
-                    <input type="text" id="passport-number" name="passport-number" placeholder="Номер паспорта" />
+                    <input type='date' id='dateBirthDay' onChange={(e)=>{
+                        let a = e.target.value.split('-').join('');
+                        setDateOfBirthday(a)
+                    }}/>
+                    <input type="text" id="passport-series" onChange={(e)=>setPassportSerial(e.target.value)} name="passport-series" placeholder="Серия паспорта" />
+                    <input type="text" id="passport-number" onChange={(e) => setPassportNumber(e.target.value)} name="passport-number" placeholder="Номер паспорта" /> 
                 </div>
-                <input type="text" id="inps" name="inps" placeholder="ИНПС" />
-                <button type="button" class="inps-button">Определить ИНПС</button>
+                <input type="text" id="inps" value={inps} name="inps" placeholder="ИНПС" />
+                <button type="button" onClick={() => getData()} class="inps-button">Определить ИНПС</button>
                 <div class="file_select">
                     
                     <label for="file-input">CV</label>
@@ -72,7 +149,7 @@ function AddEmployee() {
 
                 </div>
 
-                <button type="submit" class="submit-button" onClick={() => { console.log(file1Base64String); }}>Отправить</button>
+                <button type="submit" class="submit-button" onClick={() => postData()}>Отправить</button>
             </div>
 
             {/* <div id="modal" class="modal">
@@ -85,5 +162,79 @@ function AddEmployee() {
         </div>
     )
 }
+
+
+const Modal = ({ onClose }) => {
+    return (
+        <div style={{
+            position: 'fixed',
+            top: '20%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            backgroundColor: 'red',
+            padding: '10px',
+            color: "white",
+            zIndex: 1000,
+            borderRadius: '10px'
+        }}>
+            <p>Загрузка, получение данных</p>
+        </div>
+    );
+};
+
+const ModalSuccess = ({ onClose }) => {
+    return (
+        <div style={{
+            position: 'fixed',
+            top: '20%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            backgroundColor: 'red',
+            padding: '10px',
+            color: "green",
+            zIndex: 1000,
+            borderRadius: '10px'
+        }}>
+            <p>Документ успешно создался</p>
+        </div>
+    );
+};
+
+const ModalLoading = ({ onClose }) => {
+    return (
+        <div style={{
+            position: 'fixed',
+            top: '20%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            backgroundColor: 'red',
+            padding: '10px',
+            color: "orange",
+            zIndex: 1000,
+            borderRadius: '10px'
+        }}>
+            <p>Загрузка</p>
+        </div>
+    );
+};
+
+const ModalInfo= ({ onClose }) => {
+    return (
+        <div style={{
+            position: 'fixed',
+            top: '20%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            backgroundColor: 'red',
+            padding: '10px',
+            color: "orange",
+            zIndex: 1000,
+            borderRadius: '10px'
+        }}>
+            <p>Заполнены не все поля</p>
+        </div>
+    );
+};
+
 
 export default AddEmployee
